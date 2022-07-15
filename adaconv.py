7e7797c9ff2cf499267bb1ec25a99faaa8f52516
ioahinthemac <pioneers@snu.ac.kr>
2022-07-15 22:42:15 +0900

    initial

--- adaconv.py
+++ adaconv.py
@@ -1,1 +1,44 @@
-from
+from math import ceil, floor
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class AdaConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, n_groups=None):
+        super(AdaConv2d, self).__init__()
+        self.n_groups = in_channels if n_groups is None else n_groups
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        padding = (kernel_size - 1) / 2
+        self.conv = nn.Conv2d(in_channels=in_channels,
+                              out_channels=out_channels,
+                              kernel_size=(kernel_size, kernel_size),
+                              padding=(ceil(padding), floor(padding)),
+                              padding_mode='reflect')
+
+    def forward(self, x, w_spatial, w_pointwise, bias):
+        assert len(x) == len(w_spatial) == len(w_pointwise) == len(bias)
+        x = F.instance_norm(x)
+
+        ys = []
+        for i in range(len(x)):
+            y = self.forward_single(x[i:i+1], w_spatial[i], w_pointwise[i], bias[i])
+            ys.append(y)
+        ys = torch.cat(ys, dim=0)
+
+        ys = self.conv(ys)
+        return ys
+
+    def forward_single(self, x, w_spatial, w_pointwise, bias):
+        # Only square kernels
+        assert w_spatial.size(-1) == w_spatial.size(-2)
+        padding = (w_spatial.size(-1) - 1) / 2
+        pad = (ceil(padding), floor(padding), ceil(padding), floor(padding))
+
+        x = F.pad(x, pad=pad, mode='reflect')
+        x = F.conv2d(x, w_spatial, groups=self.n_groups)
+        x = F.conv2d(x, w_pointwise, groups=self.n_groups, bias=bias)
+        return x
