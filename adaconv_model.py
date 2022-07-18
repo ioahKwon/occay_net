724735dbd7b5883c8440e7290d9ad9f1a31f79f8
ioahinthemac <pioneers@snu.ac.kr>
2022-07-18 15:13:19 +0900

    second

--- adaconv_model.py
+++ adaconv_model.py
@@ -1,12 +1,103 @@
 from torch import nn
+from torch.nn import functional as F
+from math import ceil, floor
 
-from adaconv import *
-from kernel_predictor import *
-from ###
+from blocks import *
 
-class AdaConvModel(nn.Module):
-    def __init__(self, style_size, style_channels, kernel_size):
-        super(AdaConvModel).__init__()
-        self.encoder = ###
+# Decode Module
+class AdaOctConv(nn.Module):
+    def __init__(self, content_embeddings, style_embeddings):
+        super(AdaOctConv).__init__()
+        self.kernelPredictor = KernelPredictor()
+        self.AdaConv = AdaConv2d()
+        self.AttnOctConv = Attn_OctConv()
 
-        style_in_shape = (self.encoder.out_channels, style_size // self.encoder.scale_factor, style_size 
+    def forward(self, content, style):
+        style_v = self.kernelPredictor(style)
+        out = self.AdaConv(content, style_v)
+        out = self.AttnOctConv(out)
+        return out
+
+class KernelPredictor(nn.Module):
+    def __init__(self, in_channels, out_channels, n_groups, style_channels, kernel_size):
+        super(KernelPredictor, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.w_channels = style_channels
+        self.n_groups = n_groups
+        self.kernel_size = kernel_size
+
+        padding = (kernel_size - 1) / 2
+        self.spatial = nn.Conv2d(style_channels,
+                                 in_channels * out_channels // n_groups,
+                                 kernel_size = kernel_size,
+                                 padding = (ceil(padding), ceil(padding)),
+                                 padding_mode='reflect')
+        self.pointwise = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1,1)),
+            nn.Conv2d(style_channels,
+                      out_channels * out_channels // n_groups,
+                      kernel_size=1)
+        )
+        self.bias = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1,1)),
+            nn.Conv2d(style_channels,
+                      out_channels,
+                      kernel_size=1)
+        )
+
+    def forward(self, w):
+        w_spatial = self.spatial(w)
+        w_spatial = w_spatial.reshape(len(w),
+                                      self.out_channels,
+                                      self.in_channels // self.n_groups,
+                                      self.kernel_size, self.kernel_size)
+
+        w_pointwise = self.pointwise(w)
+        w_pointwise = w_pointwise.reshape(len(w),
+                                          self.out_channels,
+                                          self.out_channels // self.n_groups,
+                                          1, 1)
+        bias = self.bias(w)
+        bias = bias.reshape(len(w),
+                            self.out_channels)
+
+        return w_spatial, w_pointwise, bias
+
+class AdaConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, n_groups=None):
+        super(AdaConv2d, self).__init__()
+        self.n_groups = in_channels if n_groups is None else n_groups
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        padding = (kernel_size - 1) / 2
+        self.conv = nn.Conv2d(in_channels=in_channels,
+                              out_channels=out_channels,
+                              kernel_size=(kernel_size, kernel_size),
+                              padding=(ceil(padding), floor(padding)),
+                              padding_mode='reflect')
+
+    def forward(self, x, w_spatial, w_pointwise, bias):
+        assert len(x) == len(w_spatial) == len(w_pointwise) == len(bias)
+        x = F.instance_norm(x)
+
+        ys = []
+        for i in range(len(x)):
+            y = self.forward_single(x[i:i+1], w_spatial[i], w_pointwise[i], bias[i])
+            ys.append(y)
+        ys = torch.cat(ys, dim=0)
+
+        ys = self.conv(ys)
+        return ys
+
+    def forward_single(self, x, w_spatial, w_pointwise, bias):
+        # Only square kernels
+        assert w_spatial.size(-1) == w_spatial.size(-2)
+        padding = (w_spatial.size(-1) - 1) / 2
+        pad = (ceil(padding), floor(padding), ceil(padding), floor(padding))
+
+        x = F.pad(x, pad=pad, mode='reflect')
+        x = F.conv2d(x, w_spatial, groups=self.n_groups)
+        x = F.conv2d(x, w_pointwise, groups=self.n_groups, bias=bias)
+        return x
